49a7f02cc401bb42dddd9ba09e1d430dec9526fe
domq077 <dominik.smutek.51@onet.pl>
2021-04-08 20:07:03 +0200

    add currency context

---
+++ src/contexts/CurrencyContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react';
+
+export const CurrencyContext = createContext();
+
+const CurrencyContextProvider = (props) => {
+    const [currency, setCurrency] = useState([
+        {name: 'bitcoin', value: '50 000$', id: 1},
+        {name: 'ethereum', value: '30 000$', id: 2}
+    ]);
+
+    return (
+        <CurrencyContext.Provider value={{currency}}>
+            {props.children}
+        </CurrencyContext.Provider>
+    );
+}
+ 
+export default CurrencyContextProvider;
