4fd897d4dbcc5df085c9387834b630aa2a0af40a
domq077 <dominik.smutek.51@onet.pl>
2021-04-08 22:42:30 +0200

    setup failed and succes action for fetching data from api

--- src/components/CurrencyList.js
+++ src/components/CurrencyList.js
@@ -2,21 +2,27 @@ import React, { useContext } from 'react';
 import { CurrencyContext } from '../contexts/CurrencyContext';
 
 const CurrencyList = () => {
-    const { currency } = useContext(CurrencyContext);
+    const { error, isLoaded, currency } = useContext(CurrencyContext);
 
-    return (
-        <div>
-            <ul>
-                {currency.map(curr => {
-                    return (
-                        <li>
-                            <div>{curr.name}</div>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
-    );
+    if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+            <div>
+                <ul>
+                    {currency.map(curr => {
+                        return (
+                            <li>
+                                <div>{curr.name}</div>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+      }
 }
  
 export default CurrencyList;
