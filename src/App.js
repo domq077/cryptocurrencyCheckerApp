a85c893dd5867e77e3a691bef02275bdaeffd5e7
domq077 <dominik.smutek.51@onet.pl>
2021-04-09 18:49:46 +0200

    create Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CurrencyList from './components/CurrencyList';
+import Header from './components/Header';
 import CurrencyContextProvider from './contexts/CurrencyContext';
 
 function App() {
   return (
     <div className="App">
       <CurrencyContextProvider>
+        <Header />
         <CurrencyList />
       </CurrencyContextProvider>
     </div>
