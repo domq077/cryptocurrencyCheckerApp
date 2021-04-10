6872e9dd1f6bffe59d67fe49a24d7cad892aa8f7
domq077 <dominik.smutek.51@onet.pl>
2021-04-10 21:44:23 +0200

    create routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import CurrencyList from './components/CurrencyList';
 import Header from './components/Header';
+import ItemDetails from './components/ItemDetails';
 import CurrencyContextProvider from './contexts/CurrencyContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <CurrencyContextProvider>
-        <Header />
-        <CurrencyList />
-      </CurrencyContextProvider>
-    </div>
+    <Router>
+      <div className="App">
+        <CurrencyContextProvider>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={CurrencyList} />
+            <Route path="/details/:id" component={ItemDetails} />
+          </Switch>
+        </CurrencyContextProvider>
+      </div>
+    </Router>
   );
 }
 
