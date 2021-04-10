96582213680ce5cf895657515e273b63a882311a
domq077 <dominik.smutek.51@onet.pl>
2021-04-10 12:43:14 +0200

    add style to app title

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React, {  } from 'react';
 const Header = () => {
     return (
         <div>
-            <h1>Cryptocurrency List</h1>
+            <h1 className="title">Cryptocurrency List</h1>
         </div>
     );
 }
