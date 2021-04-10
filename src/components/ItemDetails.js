0496f7cbf20e2bffe33fdacf41e95f60e416bac1
domq077 <dominik.smutek.51@onet.pl>
2021-04-10 22:22:18 +0200

    fetch data for exact currency

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const ItemDeatils = () => {
+const ItemDeatils = ({ match }) => {
+    useEffect(() => {
+        fetchItem();
+    }, [])
+
+    const [item, setItem] = useState([]);
+
+    const fetchItem = async () => {
+        const fetchItem = await fetch(`https://api.coingecko.com/api/v3/coins/${match.params.id}?localization=false&developer_data=false&sparkline=true`);
+        const item = await fetchItem.json();
+        setItem(item);
+        console.log(item)
+    };
+
     return (
         <div>
-            <h1>Item details...</h1>
+            <h1>{item.name}</h1>
+            <p>{item.symbol}</p>
         </div>
     );
 }
