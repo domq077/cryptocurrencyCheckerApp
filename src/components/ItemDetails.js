3cd79b8cb1ae32ddae7f60855dd5f50aedebff07
domq077 <dominik.smutek.51@onet.pl>
2021-04-10 21:45:45 +0200

    create ItemDetails component

---
+++ src/components/ItemDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemDeatils = () => {
+    return (
+        <div>
+            <h1>Item details...</h1>
+        </div>
+    );
+}
+ 
+export default ItemDeatils;
