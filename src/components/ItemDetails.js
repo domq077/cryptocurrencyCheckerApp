071c96b10b18f6f9275dead591021a7d4168f76c
domq077 <dominik.smutek.51@onet.pl>
2021-04-11 18:57:45 +0200

    get more data from api - ItemDetails component

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -7,7 +7,11 @@ const ItemDeatils = ({ match }) => {
     const [isLoaded, setIsLoaded] = useState(false);
     const [item, setItem] = useState({
         image: false,
-        market_data: false
+        market_data: {
+            current_price: false,
+            high_24h: false,
+            low_24h: false
+        }
     });
     const history = useHistory();
     useEffect(() => {
@@ -17,7 +21,7 @@ const ItemDeatils = ({ match }) => {
                 (result) => {
                     setIsLoaded(true);
                     setItem(result);
-                    console.log(item)
+                    console.log(item);
                 },
                 (error) => {
                     setIsLoaded(true);
@@ -37,7 +41,10 @@ const ItemDeatils = ({ match }) => {
                 <img src={item.image.large}/>
                 <p>Symbol: {item.symbol}</p>
                 <p>Genesis date: {item.genesis_date}</p>
-                <p>Price change in 24h: {item.market_data.price_change_24h}</p>
+                <p>Current price: {item.market_data.current_price.usd}$</p>
+                <p>Price change in last 24h: {item.market_data.price_change_24h}$</p>
+                <p>High value in 24h: {item.market_data.high_24h.usd}$</p>
+                <p>Low value in 24h: {item.market_data.low_24h.usd}$</p>
                 <button onClick={() => history.goBack()}>Back to list</button>
             </div>
         );
