7419588429146e0b444589423f683143a698e178
domq077 <dominik.smutek.51@onet.pl>
2021-04-11 11:51:53 +0200

    add back to homepage button

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 const ItemDeatils = ({ match }) => {
     useEffect(() => {
@@ -6,6 +7,7 @@ const ItemDeatils = ({ match }) => {
     }, [])
 
     const [item, setItem] = useState([]);
+    const history = useHistory();
 
     const fetchItem = async () => {
         const fetchItem = await fetch(`https://api.coingecko.com/api/v3/coins/${match.params.id}?localization=false&developer_data=false&sparkline=true`);
@@ -16,8 +18,10 @@ const ItemDeatils = ({ match }) => {
 
     return (
         <div>
-            <h1>{item.name}</h1>
-            <p>{item.symbol}</p>
+            <h1>Name: {item.name}</h1>
+            <p>Symbol: {item.symbol}</p>
+            <p>Genesis date: {item.genesis_date}</p>
+            <button onClick={() => history.goBack()}>Back to list</button>
         </div>
     );
 }
